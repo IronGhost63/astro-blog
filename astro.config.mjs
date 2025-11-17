349303dd664b01d1a10e51c74f8c7756bc1b2b46
Jay <me@jirayu.in.th>
2025-11-17 21:59:37 +0700

    GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+// import cookieconsent  from '@jop-software/astro-cookieconsent';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	vite: {
+    site: 'https://jirayu.in.th',
+    integrations: [
+      mdx(),
+      sitemap(),
+      partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      })
+    ],
+    vite: {
     plugins: [tailwindcss()],
   },
 });
